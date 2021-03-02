d9ba00c2e9583d7ff534784b2ce07bb8069cf7ec
Nicolas Guyon <nicolas.guyon@telecomnancy.net>
2021-03-02 17:06:49 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+module.exports = () => ({
+    plugins: [
+        [
+            'module-resolver',
+            {
+                root: ['./'],
+                extensions: [
+                    '.ios.ts',
+                    '.android.ts',
+                    '.ts',
+                    '.ios.tsx',
+                    '.android.tsx',
+                    '.tsx',
+                    '.jsx',
+                    '.js',
+                    '.json',
+                    '.png',
+                ],
+            },
+        ],
+    ],
+});
+
